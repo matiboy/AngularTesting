7b1e1c0ffe6f3e2e031544435459c96dff332082
Mathieu <mathieu@redapesolutions.com>
2013-11-02 15:51:14 +0800

    Better subdivision

--- test/spec/services/Alert.js
+++ test/spec/services/Alert.js
@@ -1,6 +1,6 @@
 'use strict';
 
-xdescribe('Service: Alert', function () {
+describe('Service: Alert', function () {
   // load the service's module
   beforeEach(module('AngularTestingApp'));
 
@@ -18,45 +18,51 @@ xdescribe('Service: Alert', function () {
     expect(!!AlertService).toBe(true);
   });
 
-  it('should add an alert when add is called', function () {
-    var n = AlertService.alerts.length;
-    AlertService.add('error', 'marriage');
-    expect(AlertService.alerts.length).toEqual(n+1);
+  describe('adding alert', function() {
+    it('should add an alert when add is called', function () {
+      var n = AlertService.alerts.length;
+      AlertService.add('error', 'marriage');
+      expect(AlertService.alerts.length).toEqual(n+1);
+    });
+
+    it('should have the correct alert as the last entry', function () {
+      var n = AlertService.alerts.length;
+      AlertService.add('error', 'jquery');
+      var last = AlertService.alerts[n];
+      expect(last.type).toEqual('error');
+      expect(last.message).toEqual('jquery');
+    });
   });
 
-  it('should have the correct alert as the last entry', function () {
-    var n = AlertService.alerts.length;
-    AlertService.add('error', 'jquery');
-    var last = AlertService.alerts[n];
-    expect(last.type).toEqual('error');
-    expect(last.message).toEqual('jquery');
+  describe('removing alerts', function() {
+    it('should have one less alert when removed', function() {
+      AlertService.add('error', 'jquery');
+      AlertService.add('warning', 'someunique warning');
+      AlertService.add('warning', 'another warning');
+      var n = AlertService.alerts.length;
+      var sepc = AlertService.alerts[1]; // Pick one
+      AlertService.remove(sepc);
+      expect(AlertService.alerts.length).toEqual(n-1);
+    });
+
+    it('should remove a particular alert when asked', function() {
+      var n = AlertService.alerts.length;
+      AlertService.add('error', 'jquery');
+      AlertService.add('warning', 'someunique warning');
+      AlertService.add('warning', 'another warning');
+      var sepc = AlertService.alerts[n+1]; // this will give us the second one added
+      AlertService.remove(sepc);
+      expect(_.find(AlertService.alerts, sepc)).toBeUndefined();
+    });
   });
 
-  it('should have one less alert when removed', function() {
-    AlertService.add('error', 'jquery');
-    AlertService.add('warning', 'someunique warning');
-    AlertService.add('warning', 'another warning');
-    var n = AlertService.alerts.length;
-    var sepc = AlertService.alerts[1]; // Pick one
-    AlertService.remove(sepc);
-    expect(AlertService.alerts.length).toEqual(n-1);
-  });
-
-  it('should remove a particular alert when asked', function() {
-    var n = AlertService.alerts.length;
-    AlertService.add('error', 'jquery');
-    AlertService.add('warning', 'someunique warning');
-    AlertService.add('warning', 'another warning');
-    var sepc = AlertService.alerts[n+1]; // this will give us the second one added
-    AlertService.remove(sepc);
-    expect(_.find(AlertService.alerts, sepc)).toBeUndefined();
-  });
-
-  it('should clear all entries when clear is called', function() {
-    AlertService.add('error', 'jquery');
-    AlertService.add('warning', 'someunique warning');
-    AlertService.add('warning', 'another warning');
-    AlertService.clear();
-    expect(AlertService.alerts.length).toEqual(0);
+  describe('clearing', function() {
+    it('should clear all entries when clear is called', function() {
+      AlertService.add('error', 'jquery');
+      AlertService.add('warning', 'someunique warning');
+      AlertService.add('warning', 'another warning');
+      AlertService.clear();
+      expect(AlertService.alerts.length).toEqual(0);
+    });
   });
 });
